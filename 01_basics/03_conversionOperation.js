03c62b6aeb0dcc91ca26708f562f4e9c4c331bd9
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-10 16:37:54 +0300

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,7 +3,7 @@ let score = null
 //console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33.33" => Nan Not an Number
@@ -21,5 +21,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********** Operations **********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3 + 3);
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 / 3);
+// console.log(3 % 3); // remainder
+// console.log(3 ** 3); // power
+// console.log(3 ** 0.5); // square root
+// console.log(3 ** -1); // 1/3
+// console.log(3 ** -2); // 1/9
+// console.log(3 ** -3); // 1/27
+// console.log(3 ** -4); // 1/81
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 3
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter);

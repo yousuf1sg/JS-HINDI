1a1b7606121516528863253e7838b9683a8492d4
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-10 14:45:35 +0300

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "yousuf@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "1234567"
+accountCity = "Delhi"
+let accountState;
+
+console.log(accountId);
+console.table([ accountId, accountEmail, accountPassword, accountCity. accountState ]);
+
+
+/*
+Prefer not to use var
+because of issue in block scope
+
+Command palette Ctrl+ Shft+ P
+
+terminal  Ctrl`
+
+{} is called scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello World"); // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readble shoube be high
+
+    console.log("Yousuf") 
+
+    let name = "Yousuf"
+    let aga = 23
+    let isLoggedIn = false
+    // number => 2 to power 53
+    // bigint 
+    // string => "", ''
+    // boolean => true, false
+    // null => null standalone value
+    // undefined => undefined
+    // symbol => unique value
+    // object => key value pair
+    // function => reusable code

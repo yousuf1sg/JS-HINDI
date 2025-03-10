8fe5ed55d5fe626fc965b866c1ceaf40fa5d2a6c
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-10 16:00:52 +0300

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = null
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33.33" => Nan Not an Number
+// true => 1; fales => 0
+
+let isLoggedIn = "Yousuf"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 = false
+// "Yousuf" => true; 
+// "" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

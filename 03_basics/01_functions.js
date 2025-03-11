797e29084c13ca3acdf3a48f31b633127eecad35
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-11 23:37:01 +0300

    functions 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,6 +42,36 @@ function loginUserMessage(username = sam) {
 }
 
 //console.log(loginUserMessage("Hitesh")) // output: Hitesh just logged in
-console.log(loginUserMessage("Hitesh")) // output: undefined just logged in
+//console.log(loginUserMessage("Hitesh")) // output: undefined just logged in
 
+function calculateCartPrice(...num1) {
+    return num1 
 
+}
+
+//console.log(calculateCartPrice(200, 400, 500, 2000)) // output: 2  
+
+const user = {
+    username: "Hitesh",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 299  
+}) 
+
+const myNewArray = [200, 400, 600 ]
+    
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray)) // output: 400   ;
+
+console.log(returnSecondValue([100, 200, 300])) // output: 200   ;  // this is how to call function

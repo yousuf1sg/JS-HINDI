85e753a0cfc39572e21cc05a01d9dc1c09d8b7a1
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-12 00:39:25 +0300

    Hoisting

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -10,13 +10,53 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a)
+   //console.log("INNER: ", a)
 } // curly bracke is called scope
 
-for(let i = 0; i < array.length; i++){
-    const element = array[1]
-}
 
-console.log(a)
+// for(let i = 0; i < array.length; i++){
+//     const element = array[1]
+// }
+
+//console.log(a)
 //console.log(b)
 // console.log(c)
+
+function one(){
+    const username = "hitesh"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+}
+
+one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//+++++++++++  interesting +++++++++++
+
+function addone(num) {
+    return num + 1
+}
+
+console.log(addone(5))
+
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+addTwo(5)

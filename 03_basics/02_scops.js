d0caf2d3931da6caa01bdb987fbe6e55c8ea5127
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-11 23:50:47 +0300

    scope

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,22 @@
+// there are 3 types of objects in javascript
+
+
+
+
+//var c = 30
+// console.log(a, b, c)
+
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a)
+} // curly bracke is called scope
+
+for(let i = 0; i < array.length; i++){
+    const element = array[1]
+}
+
+console.log(a)
+//console.log(b)
+// console.log(c)

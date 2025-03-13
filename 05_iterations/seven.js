caa563aa84df8faccd3b663168dcf5e7aa04956a
Yousuf <106602949+yousuf1sg@users.noreply.github.com>
2025-03-13 11:20:48 +0300

    .map and filter

---
+++ 05_iterations/seven.js
@@ -0,0 +1,18 @@
+// map call back function
+
+const  myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.map( (num) => {return num + 10}) // with scope
+// console.log(newNums);
+
+//const newNums = myNums.map( (num) => num + 10)
+
+
+const newNums = myNums
+                .map((num) => num * 10)
+                .map((num) => num +1)
+                .filter((num) => num >= 40)
+
+console.log(newNums);
+
+
